3577f42e632b072706790c437208bf8cce8bd4c4
Владислав Черников <chernykovv@gmail.com>
2024-05-28 15:19:05 -0700

    Remove extra module in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -101,9 +101,7 @@ RSpec.configure do |config|
 =end
 end
 
-module VcrHelper
-  VCR.configure do |config|
-    config.cassette_library_dir = "fixtures/vcr_cassettes"
-    config.hook_into :webmock
-  end
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
 end
